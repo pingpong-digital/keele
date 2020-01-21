3c0747625200ef2780fd6ae93cba452d23a51361
Ben <frederick036@gmail.com>
2020-01-21 16:39:20 +0000

    Contact info

--- wp-content/themes/pingpong/footer.php
+++ wp-content/themes/pingpong/footer.php
@@ -16,7 +16,9 @@
 		<div class="pagewidth">
 			<div class="container">
 				<h2>联系我们</h2>
-				<div class="text">大家好，我是Jennifer翟，基尔大学中国代表。关于在基尔大学学习的方方面面，你都可以通过以下联系方式联系我咨询和了解。</div>
+				<div class="text">大家好，我是Jennifer翟，基尔大学中国代表。关于在基尔大学学习的方方面面，你都可以通过以下联系方式联系我咨询和了解。<br>
+				电话：+86 15710071811<br>
+电邮：<a style="color: #fff" href="mailto:j.zhai@keele.ac.uk">j.zhai@keele.ac.uk</a></div>
 				<div class="form-container">
 					<?php echo do_shortcode('[contact-form-7 id="28" title="main-form"]'); ?>
 				</div>
